4426043cf32addc205d7738decca4a6d1c8c1257
Stefan Probst <stefanprobst@kolabnow.com>
2021-07-04 16:54:31 +0200

    feat: add cli

---
+++ README.md
@@ -0,0 +1,9 @@
+# xdm-cli
+
+Same as [remark-cli](https://github.com/remarkjs/remark/tree/main/packages/remark-cli), but with plugins for handling MDX preconfigured.
+
+I am using this for linting MDX files.
+
+## How to use
+
+Configure remark `plugins` and `settings` via `.xdmrc`, `.xdmrc.js` files, or via `xdmConfig` field in `package.json`.
---
+++ cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { createRequire } from "module";
+import start from "unified-args";
+import remark from "remark";
+import { remarkMarkAndUnravel } from "xdm/lib/plugin/remark-mark-and-unravel.js";
+import { remarkMdx } from "xdm/lib/plugin/remark-mdx.js";
+
+const require = createRequire(import.meta.url);
+const proc = require("xdm/package.json");
+const cli = require("./package.json");
+
+const processor = remark().use(remarkMdx).use(remarkMarkAndUnravel);
+const extensions = ["mdx"];
+
+start({
+  processor: processor,
+  name: proc.name,
+  description: cli.description,
+  version: [
+    proc.name + ": " + proc.version,
+    cli.name + ": " + cli.version,
+  ].join(", "),
+  pluginPrefix: proc.name,
+  presetPrefix: proc.name + "-preset",
+  packageField: proc.name + "Config",
+  rcName: "." + proc.name + "rc",
+  ignoreName: "." + proc.name + "ignore",
+  extensions: extensions,
+});
