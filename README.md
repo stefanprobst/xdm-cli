977b946f8f51b7db4d05327bf173d003fc3d42f3
Stefan Probst <stefanprobst@kolabnow.com>
2021-07-04 18:00:26 +0200

    fix: always use remark configs

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 Same as [remark-cli](https://github.com/remarkjs/remark/tree/main/packages/remark-cli), but with plugins for handling MDX preconfigured.
 
 I am using this for linting MDX files.
-
-## How to use
-
-Configure remark `plugins` and `settings` via `.xdmrc`, `.xdmrc.js` files, or via `xdmConfig` field in `package.json`. To reuse an existing `remark` config (`.remarkrc`, `.remarkrc.js` files, or `remarkConfig` field), pass the `--remark` flag.
--- cli.js
+++ cli.js
@@ -7,20 +7,11 @@ import { remarkMarkAndUnravel } from "xdm/lib/plugin/remark-mark-and-unravel.js"
 import { remarkMdx } from "xdm/lib/plugin/remark-mdx.js";
 
 const require = createRequire(import.meta.url);
-const proc = require("xdm/package.json");
+const proc = require("remark/package.json");
 const cli = require("./package.json");
 
 const processor = remark().use(remarkMdx).use(remarkMarkAndUnravel);
 const extensions = ["mdx"];
-
-let name = proc.name;
-process.argv = process.argv.filter((arg) => {
-  if (arg === "--remark") {
-    name = "remark";
-    return false;
-  }
-  return true;
-});
 
 start({
   processor: processor,
@@ -30,10 +21,10 @@ start({
     proc.name + ": " + proc.version,
     cli.name + ": " + cli.version,
   ].join(", "),
-  pluginPrefix: name,
-  presetPrefix: name + "-preset",
-  packageField: name + "Config",
-  rcName: "." + name + "rc",
-  ignoreName: "." + name + "ignore",
+  pluginPrefix: proc.name,
+  presetPrefix: proc.name + "-preset",
+  packageField: proc.name + "Config",
+  rcName: "." + proc.name + "rc",
+  ignoreName: "." + proc.name + "ignore",
   extensions: extensions,
 });
